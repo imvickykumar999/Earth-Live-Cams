0b43cba0d421424ed1748b1aa46def4259e11238
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-29 17:20:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 
 import cv2
+import sys
 
-ip = '80.32.125.254'
+url = 0
 port = '8080'
+ip = '80.32.125.254'
+
+try: url = sys.argv[1]
+except: url = f'http://{ip}:{port}/cgi-bin/faststream.jpg'
 
 # cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture(f'http://{ip}:{port}/cgi-bin/faststream.jpg')
+cap = cv2.VideoCapture(url)
 
 while True:
   ret, frame = cap.read()
@@ -16,3 +21,8 @@ while True:
     
 cap.release()
 cv2.destroyAllWindows()
+
+'''
+>>> python main.py http://158.58.130.148/mjpg/video.mjpg
+>>> python main.py http://80.32.125.254:8080/cgi-bin/faststream.jpg
+'''
