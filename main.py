13febff07b6228f143c30de7a9c54d270ee8a4f1
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-29 18:59:12 +0530

    Switzerland.mp4

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 import cv2
 import sys
 
-url = 0
 port = '8080'
 ip = '80.32.125.254'
 
 try: url = sys.argv[1]
 except: url = f'http://{ip}:{port}/cgi-bin/faststream.jpg'
 
-# cap = cv2.VideoCapture(0)
+if url == '0': url = 0
 cap = cv2.VideoCapture(url)
 
 while True:
@@ -25,4 +24,5 @@ cv2.destroyAllWindows()
 '''
 >>> python main.py http://158.58.130.148/mjpg/video.mjpg
 >>> python main.py http://80.32.125.254:8080/cgi-bin/faststream.jpg
+>>> python main.py http://212.147.38.3/mjpg/video.mjpg
 '''
